094c295d57e7efc2d78a4af382e37861955e4b17
tylerthegamer69 <sdstyler2003@gmail.com>
2021-09-16 19:24:07 +0100

    skill check drawing split into a function

--- src/skillcheck.cpp
+++ src/skillcheck.cpp
@@ -1,20 +1,22 @@
 #include <raylib.h>
 #include <cmath>
 
+#include "skillcheck.h"
+
+
+Vector2 middle;
+
 int main(void)
 {
-	const int screenWidth = 800;
-	const int screenHeight = 450;
-	const int radius = 100;
+
 
 	SetConfigFlags(FLAG_MSAA_4X_HINT);
 
 
 	int score = 0;
 
-	Vector2 middle;
 	Vector2 skillCheckZone;
-	float rotationAngle = 90.0f;
+	float rotationAngle = -90.0f;
 	bool moveSkillCheck = true;
 
 	InitWindow(screenWidth, screenHeight, "Skillcheck Simulator");
@@ -26,7 +28,8 @@ int main(void)
 
 	timer = GetTime();
 	spawnSkillcheckTimer = timer + 2;
-	int bruh = GetRandomValue(90, 90 + 180);
+	//int bruh = GetRandomValue(0, 90 + 180);
+	int bruh = GetRandomValue(0, 360);
 	skillCheckZone = { (float)bruh, (float)bruh + 15.0f };
 
 	while (!WindowShouldClose())
@@ -40,7 +43,8 @@ int main(void)
 			rotationAngle = -90.0f;
 			moveSkillCheck = true;
 			spawnSkillcheckTimer = timer + 2;
-			int rand = GetRandomValue(90, 90 + 180);
+			//int rand = GetRandomValue(0, 90 + 180);
+			int rand = GetRandomValue(0, 360);
 			skillCheckZone = { (float)rand, (float)rand + 15.0f };
 		}
 
@@ -64,24 +68,9 @@ int main(void)
 
 		BeginDrawing();
 
-			ClearBackground(RAYWHITE);
+			ClearBackground(GRAY);
+			DrawSkillCheck(rotationAngle, Vector2{ 0,20 });
 
-			DrawCircleLines(middle.x, middle.y, 100, BLACK);
-
-			DrawRing(middle, radius-5, radius+5, skillCheckZone.x, skillCheckZone.y, 15, GREEN);
-
-
-			DrawLineEx
-			(
-				middle, 
-				Vector2
-				{
-					middle.x + cosf(rotationAngle * DEG2RAD)*radius,
-					middle.y + sinf(rotationAngle * DEG2RAD)*radius
-				}, 
-				6.0f,
-				RED
-			);
 		EndDrawing();
 	}
 
@@ -89,4 +78,21 @@ int main(void)
 	return 0;
 }
 
+void DrawSkillCheck(float angle, Vector2 skillCheckZone) 
+{
+	DrawCircleLines(middle.x, middle.y, 100, BLACK);
+	DrawRing(middle, radius - 5, radius + 5, skillCheckZone.x, skillCheckZone.y, 15, GREEN);
+	DrawLineEx(
+		middle,
+		Vector2
+		{
+			middle.x + cosf(angle * DEG2RAD) * radius,
+			middle.y + sinf(angle * DEG2RAD) * radius
+		},
+		5.0f,
+		RED
+	);
+		
+
+}
 
---
+++ src/skillcheck.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <raylib.h>
+
+void DrawSkillCheck(float angle, Vector2 skillCheckZone);
+const int screenWidth = 800;
+const int screenHeight = 450;
+const int radius = 100;
+
+extern Vector2 middle;
