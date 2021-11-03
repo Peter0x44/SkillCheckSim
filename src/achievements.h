f73e9ef82dee7f76c1b1aa95d5956381a7ecffbe
Peter0x44 <peter0x44@snopyta.org>
2021-11-03 08:47:22 +0000

    Split achievement descriptions in to separate array of strings

--- src/achievements.h
+++ src/achievements.h
@@ -9,38 +9,43 @@ class achievementscreen : public gamestate
 private:
 	stats scores;
 	Rectangle Backboard = { screenWidth / 2 - 375, screenHeight / 2 - 188, 750, 350 };
-	//Rectangle Backboard = { screenWidth / 8, screenHeight / 40, screenWidth * (2/3), screenHeight * (2) };
 
-	//const int AchievementNumber = 8;
-
-	const char* const achievements[16] = {
-		"hit 10 great skill checks in a row",
-		"miss 50 skillchecks in a row",
-		"Hit 50 great skillchecks",
-		"Miss 10 skillchecks in a row", //ARRAY OF ACHIEVEMENTS
-		"hit 50 greatskillchecks in a row",
-		"hit 200 great skill checks in a row",
-		"hit 500 great skill checks overall",
+	const char* const achievementCondition[16] = {
+		"Get 7000 Bloodpoints",
+		"Get 32000 Bloodpoints",
+		"Hit 50 great skill checks",
+		"Hit 100 great skill checks",
+		"Hit 500 great skill checks",
+		"Fail 50 skill checks",
+		"Fail 100 skill checks",
+		"Fail 500 skill checks",
+		"Fail 1000 skill checks",
+		"Hit 5 great skill checks in a row",
+		"Hit 10 great skill checks in a row ",
+		"Play 1000 skill checks",
+		"Play 2000 skill checks",
+		"Play 3000 skill checks ",
+		"Play 5000 skill checks",
 		"???"
 	};
 
 	const char* const achievementTitle[16] = {
-		"You pip'd so you had fun: Get 7000 Bloodpoints",
-		"Farming : MAX: Get 32000 Bloodpoints",
-		"Level up: Hit 50 great skill checks",
-		"Trust me: Hit 100 great skill checks",
-		"I'm a engineer: Hit 500 great skill checks",
-		"Brand New Dwight: Fail 50 skill checks",
-		"Blame the lag: Fail 100 skill checks",
-		"Well deserved facecamp: Fail 500 skill checks",
-		"-rep Please uninstall: Fail 1000 skill checks",
-		"Not even immersed: Hit 5 great skill checks in a row",
-		"Best player in the world: Hit 10 great skill checks in a row ",
-		"Now play the actual game: Play 1000 skill checks",
-		"What in tarnation ?: Play 2000 skill checks",
-		"Infinite abuser: Play 3000 skill checks ",
-		"Esport ready: Play 5000 skill checks",
-		"Blame Mclean: ???"
+		"You pip'd so you had fun",
+		"Farming : MAX",
+		"Level up",
+		"Trust me",
+		"I'm a engineer",
+		"Brand New Dwight",
+		"Blame the lag",
+		"Well deserved facecamp",
+		"-rep Please uninstall",
+		"Not even immersed",
+		"Best player in the world",
+		"Now play the actual game",
+		"What in tarnation?",
+		"Infinite abuser",
+		"Esport ready",
+		"Blame Mclean",
 	};
 
 	const Rectangle backbutton = { screenWidth - 50, screenHeight - 50, 50, 50 }; //BACK BUTTON IS DRAWN
@@ -50,22 +55,10 @@ private:
 		false
 	};
 
-	//bool hit10skillchecksinarow = false;
-	//bool miss50skillchecksinarow = false;
-	//bool hit50greatskillchecks = false;
-	//bool miss10skillchecksinarow = false;
-	//bool hit50greatskillchecksinarow = false;
-	//bool hit200greatskillchecksinarow = false;
-	//bool hit500greatskillchecksoverall = false;
-	//bool questionmark = false;
-
 	const int gridOffsetX = (screenWidth - 68 * 7) / 2;
 	const int gridOffsetY = 55;
 	int achievementshoveredx;
 	int achievementshoveredy;
-
-
-
 
 public:
 	void logic(void) final override;
